ba4e96155952881dac91d3931f82814b49fec8f2
Brendan Ketelsen <bk45@iastate.com>
2023-03-21 17:15:37 -0500

    Beginning of Bad ending

---
+++ badEndingP1.java
@@ -0,0 +1,12 @@
+public class badEndingP1 {
+    public static void main(String[] args){
+        System.out.println("You leave the sword and armor where it is deciding it's ugly and doesn't suit you and stash the .");
+        System.out.println("As you walk away from the chest a dark figure in the woods wispers at you to come here.");
+        System.out.println("You walk over to the man to hear what he has to say.");
+        System.out.println("The man introduces himself as Lilitu.");
+        System.out.println("He then explains to you about the horrors of the dragon who rules over Inkarnate.");
+        System.out.println("Liitu then offers you a special dark sword that he claims will be able to sly the dragon.");
+        System.out.println("He convinces you to take the sword and slay the dragon and become the new ruler of Inkarnate.");
+        System.out.println("You head for your farm to begin your quest to slay the dragon.");
+    }
+}
