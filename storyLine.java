9638f015ddc94a9b905220ea34ba346ac8a0d9b6
cjbebee <cjbebee@iastate.edu>
2023-03-21 16:55:55 -0500

    Methods for the storyline and post battle info

---
+++ storyLine.java
@@ -0,0 +1,161 @@
+import java.util.Scanner;
+import java.Util;
+
+public class storyLine {
+
+        public static void main(String[] args) {
+
+        }
+
+        public static void intro() {
+                System.out.println("One morning you wake up to the roosters crow on your family farm.");
+                System.out.println(
+                                "You wake up happy and go about doing your normal chores for the day by tending to the animals in the barn and then checking on the crops in the field.");
+                System.out.println(
+                                "While you are out looking through the crops you stumble across an old worn down chest.");
+                System.out.println(
+                                "After breaking it open you find a set of knights' armor and a massive sword inside it.");
+                System.out.println(
+                                "You get happy making this lucky find, however its short lived as you have to finish your chores around the farm, for the mean time you put the chest in your house.");
+                System.out.println();
+        }
+
+        public static void mainEvent1() {
+                System.out.println(
+                                "After working on the chores around the farm you decide to head to bed for the night, storing the chest underneath your floorboards that way its hidden.");
+                System.out.println("While peacefully sleeping, you smell burning wood.");
+                System.out.println(
+                                "As you jolt upright from your bed you rush outside to see a bunch of bandits throwing torches at your house.");
+                System.out.println(
+                                "As soon as you exit your house you get hit in the head by a club and begin to blackout.");
+                System.out.println(
+                                "As you fade out of conciousness you yell at one of the bandits as they loot your house for all your belongings");
+                System.out.println(
+                                "You yell at the Bandit why are you doing this to which he replies by pointing at the sky.");
+                System.out.println(
+                                "you roll onto your back and look at the sky to see a massive figure break throught the clouds and douse your homestead in the intense heat of fire");
+                System.out.println("As the flames eat away your beautiful home you pass out in the burning rubble.");
+                System.out.println(
+                                "When you come to, you start to cry and pound the ground in rage, as you search through the rubble you manage to find some clothes and a small amount of personal belongings, unfortunenately they are burnt by the fire");
+                System.out.println(
+                                "However while searching your floorboards you find the chest fully intact completely undamaged.");
+                System.out.println("");
+
+                System.out.println("1- Would you like to take the holy sword and armor for yourself?");
+                System.out.println("2- Would you like to leave it there, I'm sure someone will come back for it.");
+                System.out.println(
+                                "3- Would you like to sell the sword and armor? I'm sure it would fetch a fancy price.");
+        }
+
+        public static void mainEvent2() {
+                System.out.println(
+                                "After making your decision, you decide the best course of action is to head to the closest town in order to get some information about the bandits that attacked your home.");
+                System.out.println(
+                                "Talespire is a town about 2 miles away from your home, as you reach the walls of the town you are exhausted by travel and the guards stop you at the gates.");
+                System.out.println(
+                                "As soon as you approach the gates the guards ask what your reason for entrance is, you exclaim that your house was burnt down and you are looking to get some info abou the ones responsible for it.");
+                System.out.println(
+                                "The guards decide to let you past, and as you walk into the town you heard a loud commotion as you see some thugs attacking a poor fruit vendor");
+                System.out.println(
+                                "You think it would be a smart decision to help the man seeing as they are bandits they might know what happened at your house. Wink Wink");
+                System.out.println("");
+
+                System.out.println("1- Do you decide to help the fruit vendor?");
+                System.out.println("2- Nah im sure he can figure it out himself");
+                System.out.println("3- I have more important matters to do (P.S. who even talks like this)");
+        }
+
+        public static void mainEvent3() {
+                System.out.println(
+                                "After defeating the bandits, and gaining some well needed information about the dragons whereabouts.");
+                System.out.println(
+                                "You decide its time to set off towards the castle at the center of all maps, Inkarnate.");
+                System.out.println("Its said that the castle is built suspended in the center of a live volcano.");
+                System.out.println(
+                                "You leave the town of Talespire and set off in the direction of Inkarnate, after traveling on foot for several hours you decide to set up camp as night falls.");
+                System.out.println(
+                                "As you finish setting up camp along the road next to the treeline, you hear some rustling of leaves as four large wolves walk into the clearing.");
+                System.out.println("The wolves foam at the mouth as they approach your camp.");
+                System.out.println("What will you do?");
+
+                System.out.println("1- Throw some food at them im sure they are hungry");
+                System.out.println("2- Prepare to fight");
+                System.out.println("3- Run...(I doubt you can outrun them, ... just an observation :)");
+
+        }
+
+        public static void mainEvent4() {
+                System.out.println("After your close call with the Wolves you set back on track towards the castle.");
+                System.out.println(
+                                "As the sun rises, as you walk along the road you see the sun bounce of the roof of a huge castle atop a huge mountain.");
+                System.out.println(
+                                "After traveling an additional five hours, you reach the base of the mountain-like volcano and start your ascent.");
+                System.out.println("As you reach the peak of the volcano, you are met with a sweltering heat");
+                System.out.println(
+                                "Looking across the volcano you see a large castle, some of the walls are melted and scarred from constant heat damage.");
+                System.out.println(
+                                "As you take in the view you come across an old rickety rope bridge, it looks just as damaged as the castle.");
+                System.out.println("Im suprised its lasted that long considering its wood and rope.");
+                System.out.println(
+                                "As you struggle to cross the bridg you can see that there are two suits of armor standing by the front entrance.");
+                System.out.println(
+                                "As you finally manage to get across the bridge, you step towards the door, but as you do you get blocked by two swords");
+                System.out.println(
+                                "The guards shift there heads towards you and in an acho like drone they tell you that you can't enter");
+
+                System.out.println("1- Defeat the guards");
+                System.out.println("2- Turn away and run");
+                System.out.println("3- Move forward to rescue the princess");
+        }
+
+        public static void mainEvent5() {
+                System.out.println("Once you enter the castle, you see a massive spiral staircase ahead of you.");
+                System.out.println(
+                                "As you walk towards the stairs you hear a scream come from above, followed by the roar of the dragon.");
+                System.out.println("As you ascend the steps the sound of beating wings can be heard from outside.");
+                System.out.println(
+                                "As you reach the top you walk into a bedroom with a beautidul woman screaming and pointing at the window");
+                System.out.println("You look through the window and see a massive eyeball staring into your soul");
+                System.out.println(
+                                "Before you can react you see a claw grab the top of the window, just as quickly as you see it the roof gets torn of the top of the tower.");
+                System.out.println(
+                                "As the princess scrambles to get away, the dragon drops the top of the tower into the lava below.");
+                System.out.println(
+                                "it turns its head towards you and bellows I am the mighty Berg I have terrorized these lands for centuries");
+                System.out.println(
+                                "Berg looks down on you and says I suppose whether your here for righteousness or not we must duel");
+                System.out.println("Berg drops down onto the edges of the tower and the fight begins");
+        }
+
+        public static void wonFight1() {
+                System.out.println(
+                                "The last bandit falls to the ground as he coughs, he looks at you and gasps saying that he was sorry aand they didnt have a choice.");
+                System.out.println(
+                                "He tells you that the dragon ordered the bandits to attack your home because you found the holy sword.");
+                System.out.println("As he slowly gasps for air he says that he can tell you where the dragon is");
+                System.out.println(
+                                "He says the dragon dwells in a castle at the center of all maps, the volcano castle Inkarnate.");
+                System.out.println(
+                                "There are many legends surroundin the castle and how it has never been successfully entered in centuries");
+                System.out.println(
+                                "The bandit then looks at you as his eyes begin to tear up, he says that you werent the only person attacked.");
+                System.out.println("To make matters worse he says that the other victim was kidnapped");
+                System.out.println(
+                                "As he gulps his last breath of air he says the dragon captured the princess, he then falls limp succumbing to his injuries.");
+        }
+
+        public static void wonFight2() {
+                System.out.println(
+                                "The last wolf falls to the ground, exhausted by combat you take a moment to clean the blood from your self and gather your camp.");
+                System.out.println(
+                                "You think that another group of wild animals could attack you in your sleep and your not taking the chance.");
+                System.out.println("");
+        }
+
+        public static void wonFight3() {
+                System.out.println(
+                                "Both guards crumple in defeat and fall against the walls of the castle.");
+                System.out.println(" you decide now is a good time to prepare yourself for what is about to happen.");
+        }
+
+}
