cb2687c7a1730c247a5a5e03ca9df8b9b34fe035
Brendan Ketelsen <bk45@iastate.com>
2023-03-21 17:16:44 -0500

    Ending of Bad ending

---
+++ badEndingP2.java
@@ -0,0 +1,12 @@
+public class badEndingP2 {
+    public static void main(String[] args){
+        System.out.println("After slaying the dragon with the dark sword you walk over to it's body and pick up it's crown.");
+        System.out.println("You put on the crown and a dark mysterious mist starts to rise from you and the sword.");
+        System.out.println("The mist then shoots into you and you start to feel more powerful and stronger than ever before.");
+        System.out.println("The first thing you do with the power is create an army of unstoppable demans.");
+        System.out.println("Then you send the army our into the land to destroy everything the dragon worked so hard for.");
+        System.out.println("You sit on the thrown that overlooks Inkarnate and listen to the destruction with gloomy skies and a grin on your face knowing you won and nothing can stop you.");
+        System.out.println();
+        System.out.println("THE END.");
+    }
+}
