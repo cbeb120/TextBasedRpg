e6028cd16543d8e47a31878fe4e8941a9eb12caf
cjbebee <cjbebee@iastate.edu>
2023-04-04 16:58:59 -0500

    added some logic for Demo2

---
+++ mainFile.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.Util;
+
+public class mainFile {
+    public static void main(String[] args) {
+        storyLine.intro();
+        System.out.println();
+        storyLine.mainEvent1();
+        storyLine.mainEvent2();
+    }
+
+}
