d13fb6a64809132a0c192d7ab63a42eef4b4284b
Brendan Ketelsen <bk45@iastate.com>
2023-04-04 16:32:42 -0500

    Beginning of bad ending

---
+++ neutralEndingP1.java
@@ -0,0 +1,10 @@
+public class neutralEndingP1 {
+    public static void main(String[] args){
+        System.out.println("As you gaze in the chest you realize how shiny and vaulable the items look.");
+        System.out.println("You think about how much money to help you on your farm and decide to go sell it at a local market.");
+        System.out.println("But the search for a buyer takes a lot longer than you expected as there are not a lot of people in need of a sword and shield.");
+        System.out.println("After hours of searching eventually you come across a small shop in the corner of the market.");
+        System.out.println("He agrees to buy the items from you for 100 gold coins which is a lot more than you expected.");
+        System.out.println("You are extatic and walk back to your farm with a giant smile on your face.");
+    }
+}
