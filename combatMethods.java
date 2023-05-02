a5d45a8b503caf0ad45ca9e7633edefb15c95df7
Ryan Martin <rymart03@iastate.edu>
2023-05-02 12:11:08 -0500

    added restoreHealth method to restore player health at beginning of every fight sequence

--- combatMethods.java
+++ combatMethods.java
@@ -1,4 +1,3 @@
-
 //java.util.Random;
 import java.util.Random;
 import java.util.Scanner;
@@ -9,310 +8,310 @@ public class combatMethods {
     private static int enemy2Health = 75;
     private static int enemy3Health = 100;
     private static int enemy4Health = 125;
-    //private int opt = 0;
-    public combatMethods(int health){
-        //health = 100;
+
+    // private int opt = 0;
+    public combatMethods(int health) {
+        // health = 100;
     }
-    public static void beenHit(){
+
+    public static void restoreHealth(){
+        health = 100;
+    }
+
+    public static void beenHit() {
         health = health - 10;
-        //System.out.println(health);
+        // System.out.println(health);
     }
-    public  static void viewHealth(){
+
+    public static void viewHealth() {
+        System.out.println();
         System.out.println("Your current health is " + health);
     }
-    public static void addArmor(){
+
+    public static void addArmor() {
         health = health + 10;
+        System.out.println();
         System.out.println(health);
     }
-    public static void viewEnemy1Health(){
+
+    public static void viewEnemy1Health() {
+        System.out.println();
         System.out.println("The enemies health is " + enemy1Health);
     }
-    public static void enemy1Attack(){
-        if(health > 0){
+
+    public static void enemy1Attack() {
+        if (health > 0) {
             health = health - 10;
+            System.out.println();
             System.out.println("The enemy hit you! You have taken damage.");
             viewHealth();
-            }
-        else{
+        } else {
             youDied();
-            }
-    }
-    public static void enemy1Hit(){
-        enemy1Health = enemy1Health - 10;
-        if( enemy1Health <= 0){
-            //battleWon();
-        }
-        else{
-            System.out.println("You have done damage to the enemy!");
-            //viewEnemy1Health();
         }
     }
-    public static void enemy1SwordHit(){
-        enemy1Health = enemy1Health - 25;
-        if( enemy1Health <= 0){
-            battleWon();
-        }
-        else{
-            System.out.println("You hit the enemy with your sword, nice hit!");
 
+    public static void enemy1Hit() {
+        enemy1Health = enemy1Health - 10;
+        if (enemy1Health <= 0) {
+            battleWon1();
+        } else {
+            System.out.println();
+            System.out.println("You have done damage to the enemy!");
             viewEnemy1Health();
         }
     }
-    public static void enemy1fight(){
 
-        while(health >= 1 && enemy1Health >= 1){
+    public static void enemy1SwordHit() {
+        enemy1Health = enemy1Health - 25;
+        if (enemy1Health <= 0) {
+            battleWon1();
+        } else {
+            System.out.println();
+            System.out.println("You hit the enemy with your sword, nice hit!");
+            viewEnemy1Health();
+        }
+    }
+
+    public static void enemy1fight() {
+        restoreHealth();
+        while (health >= 1 && enemy1Health >= 1) {
             Scanner scanner = new Scanner(System.in);
             viewHealth();
             viewEnemy1Health();
             int temp = randAtt.randomAttack1();
-            if(temp >= 1 && temp <= 10){
+            if (temp >= 1 && temp <= 10) {
                 enemy1Hit();
-            }
-            else{
+            } else {
                 beenHit();
             }
-            //enemy1Hit();
+            System.out.println();
             System.out.println("Press any key to continue the fight:");
             boolean response = scanner.hasNext();
-            if(response == true && enemy1Health > 0 && health > 0) {
+            if (response == true && enemy1Health > 0 && health > 0) {
                 continue;
             }
-            
+
         }
-        
-        
-        
+
     }
 
-    public static void enemy2fight(){
-
-        while(health >= 1 && enemy2Health >= 1){
+    public static void enemy2fight() {
+        restoreHealth();
+        while (health >= 1 && enemy2Health >= 1) {
             Scanner scanner = new Scanner(System.in);
             viewHealth();
             viewEnemy2Health();
             int temp = randAtt.randomAttack1();
-            if(temp >= 1 && temp <= 10){
+            if (temp >= 1 && temp <= 10) {
                 enemy2Hit();
-            }
-            else{
+            } else {
                 beenHit();
             }
+            System.out.println();
             System.out.println("Press any key to continue the fight:");
             boolean response = scanner.hasNext();
-            if(response == true && enemy2Health > 0 && health > 0) {
+            if (response == true && enemy2Health > 0 && health > 0) {
                 continue;
             }
-            
+
         }
-        
-        
-        
+
     }
 
-    public static void enemy3fight(){
-
-        while(health >= 1 && enemy3Health >= 1){
+    public static void enemy3fight() {
+        restoreHealth();
+        while (health >= 1 && enemy3Health >= 1) {
             Scanner scanner = new Scanner(System.in);
             viewHealth();
             viewEnemy3Health();
             int temp = randAtt.randomAttack1();
-            if(temp >= 1 && temp <= 10){
+            if (temp >= 1 && temp <= 10) {
                 enemy3Hit();
-            }
-            else{
+            } else {
                 beenHit();
             }
+            System.out.println();
             System.out.println("Press any key to continue the fight:");
             boolean response = scanner.hasNext();
-            if(response == true && enemy3Health > 0 && health > 0) {
+            if (response == true && enemy3Health > 0 && health > 0) {
                 continue;
             }
-            
+
         }
-        
-        
-        
+
     }
 
-    public static void enemy4fight(){
-
-        while(health >= 1 && enemy4Health >= 1){
+    public static void enemy4fight() {
+        restoreHealth();
+        while (health >= 1 && enemy4Health >= 1) {
             Scanner scanner = new Scanner(System.in);
             viewHealth();
             viewEnemy4Health();
             int temp = randAtt.randomAttack1();
-            if(temp >= 1 && temp <= 10){
+            if (temp >= 1 && temp <= 10) {
                 enemy4Hit();
-            }
-            else{
+            } else {
                 beenHit();
             }
+            System.out.println();
             System.out.println("Press any key to continue the fight:");
             boolean response = scanner.hasNext();
-            if(response == true && enemy4Health > 0 && health > 0) {
+            if (response == true && enemy4Health > 0 && health > 0) {
                 continue;
             }
-            
+
         }
-        
-        
-        
+
     }
-    
+
     //
-    public static void viewEnemy2Health(){
+    public static void viewEnemy2Health() {
+        System.out.println();
         System.out.println("The enemies health is " + enemy2Health);
     }
-    public static void enemy2Attack(){
-        if(health > 0){
+
+    public static void enemy2Attack() {
+        if (health > 0) {
             health = health - 10;
+            System.out.println();
             System.out.println("The enemy hit you! You have taken damage.");
             viewHealth();
-            }
-        else{
+        } else {
             youDied();
-            }
-    }
-    public static void enemy2Hit(){
-        enemy2Health = enemy2Health - 10;
-        if( enemy2Health <= 0){
-            battleWon();
         }
-        else{
-            System.out.println("You hit the enemy!");
+    }
 
+    public static void enemy2Hit() {
+        enemy2Health = enemy2Health - 10;
+        if (enemy2Health <= 0) {
+            battleWon2();
+        } else {
+            System.out.println();
+            System.out.println("You hit the enemy!");
             viewEnemy2Health();
         }
     }
-    public static void enemy2SwordHit(){
+
+    public static void enemy2SwordHit() {
         enemy1Health = enemy2Health - 25;
-        if( enemy2Health <= 0){
-            battleWon();
-        }
-        else{
+        if (enemy2Health <= 0) {
+            battleWon2();
+        } else {
             viewEnemy2Health();
         }
     }
-    //
-    public static void viewEnemy3Health(){
+
+    public static void viewEnemy3Health() {
+        System.out.println();
         System.out.println("The enemies health is " + enemy3Health);
     }
-    public static void enemy3Attack(){
-        if(health > 0){
+
+    public static void enemy3Attack() {
+        if (health > 0) {
             health = health - 10;
+            System.out.println();
             System.out.println("The enemy hit you! You have taken damage.");
             viewHealth();
-            }
-        else{
+        } else {
             youDied();
-            }
-    }
-    public static void enemy3Hit(){
-        enemy3Health = enemy3Health - 10;
-        if( enemy3Health <= 0){
-            battleWon();
         }
-        else{
-            System.out.println("You hit the enemy!");
+    }
 
+    public static void enemy3Hit() {
+        enemy3Health = enemy3Health - 10;
+        if (enemy3Health <= 0) {
+            battleWon3();
+        } else {
+            System.out.println();
+            System.out.println("You hit the enemy!");
             viewEnemy3Health();
         }
     }
-    public static void enemy3SwordHit(){
+
+    public static void enemy3SwordHit() {
         enemy3Health = enemy3Health - 25;
-        if( enemy3Health <= 0){
-            battleWon();
-        }
-        else{
+        if (enemy3Health <= 0) {
+            battleWon3();
+        } else {
             viewEnemy3Health();
         }
     }
+
     //
-    public static void viewEnemy4Health(){
+    public static void viewEnemy4Health() {
+        System.out.println();
         System.out.println("The enemies health is " + enemy4Health);
     }
-    public static void enemy4Attack(){
-        if(health > 0){
-        health = health - 10;
-        System.out.println("The enemy hit you! You have taken damage.");
-        viewHealth();
-        }
-        else{
+
+    public static void enemy4Attack() {
+        if (health > 0) {
+            health = health - 10;
+            System.out.println();
+            System.out.println("The enemy hit you! You have taken damage.");
+            viewHealth();
+        } else {
             youDied();
         }
     }
-    public static void enemy4Hit(){
+
+    public static void enemy4Hit() {
         enemy4Health = enemy4Health - 10;
-        if( enemy4Health <= 0){
-            battleWon();
-        }
-        else{
+        if (enemy4Health <= 0) {
+            battleWon4();
+        } else {
+            System.out.println();
             System.out.println("You hit the enemy!");
-
             viewEnemy4Health();
         }
     }
-    public static void enemy4SwordHit(){
+
+    public static void enemy4SwordHit() {
         enemy4Health = enemy4Health - 25;
-        if( enemy4Health <= 0){
-            battleWon();
-        }
-        else{
+        if (enemy4Health <= 0) {
+            battleWon4();
+        } else {
             viewEnemy4Health();
         }
     }
 
-
-    
-    public static boolean battleWon1(){
-        if(health > 0 && enemy1Health == 0)
-        {
+    public static boolean battleWon1() {
+        if (health > 0 && enemy1Health == 0) {
             return true;
-        }
-        else{
-            return false;
-        }
-    }
-    public static boolean battleWon2(){
-        if(health > 0 && enemy2Health == 0)
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    public static boolean battleWon3(){
-        if(health > 0 && enemy3Health == 0)
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    public static boolean battleWon4(){
-        if(health > 0 && enemy4Health == 0)
-        {
-            return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 
-    public static void youDied(){
-        if(health <= 0){
+    public static boolean battleWon2() {
+        if (health > 0 && enemy2Health == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean battleWon3() {
+        if (health > 0 && enemy3Health == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean battleWon4() {
+        if (health > 0 && enemy4Health == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void youDied() {
+        if (health <= 0) {
+            System.out.println();
             System.out.println("YOU DIED!");
         }
     }
 
-    
-    
-
-
-
-
-
-    
 }
+
